c5d25799a049bd0a13d72e729bfd964e9d877d76
schraderjp <schraderjp@gmail.com>
2022-06-12 20:48:30 -0400

    update route change animtatons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink, Route, Routes, useLocation } from "react-router-dom";
 import AnimationLayout from "./components/AnimationLayout";
 import Dashboard from "./components/pages/Dashboard";
 import Expenses from "./components/pages/Expenses";
 import Income from "./components/pages/Income";
 import Tools from "./components/pages/Tools";
+import { animated, config, useTransition } from "@react-spring/web";
 
 function App() {
+  const location = useLocation();
+  const transition = useTransition(location, {
+    from: { opacity: 0, translateX: -15 },
+    enter: { opacity: 1, translateX: 0 },
+    leave: { opacity: 0, translateX: -15 },
+    exitBeforeEnter: true,
+    config: { mass: 1, tension: 275, friction: 30 },
+
+    delay: 0,
+  });
   return (
     <div className="container mx-auto">
       <header className=" w-full bg-sky-600">
@@ -28,16 +39,16 @@ function App() {
           </NavLink>
         </div>
       </header>
-      <main className="p-1">
-        <Routes>
-          <Route element={<AnimationLayout />}>
+      {transition((props, item) => (
+        <animated.main className="p-1" style={props}>
+          <Routes location={item}>
             <Route path="/" element={<Dashboard />} />
             <Route path="/expenses" element={<Expenses />} />
             <Route path="/income" element={<Income />} />
             <Route path="/tools" element={<Tools />} />
-          </Route>
-        </Routes>
-      </main>
+          </Routes>
+        </animated.main>
+      ))}
     </div>
   );
 }
